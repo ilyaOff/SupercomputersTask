9e6d94b49b252961c2ada9a52d6596fff11454b9
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2023-11-14 22:26:05 +0300

    fix: забыл namespace

--- Task2/Task2.h
+++ Task2/Task2.h
@@ -8,4 +8,4 @@ double CalculateF(double x, double y, double h1, double h2);
 
 double MainFunction(double **w, int i, int j, int M, int N, double **a, double **b, double h1, double h2);
 
-void SaveResults(double **w, int N, int M, ofstream &fileoutput);
+void SaveResults(double **w, int N, int M, std::ofstream &fileoutput);
