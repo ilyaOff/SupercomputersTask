c0a29f26463bcfd21c23ad6a26ac45be88440358
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2023-11-19 17:57:33 +0300

    feat: высчитывание коэффициентов на хожу
    Неэффективно, пробовал менять порядок вычисления элементов в цикле и тип for shedue - всё равно 5.9 против 2.1 секунды

--- Task2/MyMacroses.h
+++ Task2/MyMacroses.h
@@ -15,13 +15,32 @@
 //left1 = i < 15 ? 2 : 5;\
 // res = -( w[i][j] + center1 + left1);};
 
-#define MainFunctionParallel2(res,f, w, i, j, M, N, a, b, h1, h2) {\
+#define MainFunctionParallel2(res,f, w, i, j, M, N, a1, a2 , b, h1, h2) {\
 	double center1, left1, right1, top1, down1;\
 	center1 = w[i][j];\
 	left1 = i - 1 == 0 ? 0 : w[i - 1][j];\
 	right1 = i + 1 == M ? 0 : w[i + 1][j];\
 	top1 = j + 1 == N ? 0 : w[i][j + 1];\
 	down1 = j - 1 == 0 ? 0 : w[i][j - 1];\
-	double dx = (a[i + 1][j] * (right1 - center1) - a[i][j] * (center1 - left1)) / (h1 * h1);\
+	double dx = (a1 * (right1 - center1) - a2 * (center1 - left1)) / (h1 * h1);\
 	double dy = (b[i][j + 1] * (top1 - center1) - b[i][j] * (center1 - down1)) / (h2 * h2);\
 	res = f -(dx + dy);};
+
+#define CalcA(res, i, j){\
+	double x = P0.X + (i)*h1 - h1 / 2;\
+	if (x <= C.X)\
+		{res = 1.0;}\
+	else{\
+		double yCB = 9 - 3 * x;\
+		double y = P0.Y + (j) * h2;\
+		double aEps = y + h2 / 2 - yCB;\
+		if (aEps < 0)\
+			{res = 1.0;}\
+		else{\
+		double aOne = yCB - (y - h2 / 2);\
+		if (aOne < 0)\
+			{res = 1.0 / epsilon;}\
+		else {res = (aEps / epsilon + aOne) / h2;}\
+		}\
+	}\
+ };
--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -128,17 +128,20 @@ int main(int argc, char **argv)
 		fout.close();
 	}
 	#endif
-
+	omp_set_num_threads(omp_get_max_threads());
 	#pragma omp parallel default(none) shared(tauNumerator, tauDenominator, deltaSqr, w, a, b, F, k) private(i, j, rA, tau)
 	for (; k < KMAX; )
 	{
 		//посчитать невязку r
-		#pragma omp  for  collapse(2) schedule(static) 
-		for (i = 1; i < M; ++i)
+		#pragma omp  for  collapse(2) schedule(guided, 2)
+		for (i = M - 1; i > 0; --i)
 		{
-			for (j = 1; j < N; ++j)
+			for (j = N - 1; j > 0; --j)
 			{
-				MainFunctionParallel2(r[i][j], -F[i][j], w, i, j, M, N, a, b, h1, h2);
+				double a1 = 0, a2 = 0;
+				CalcA(a1, i + 1, j);
+				CalcA(a2, i, j);
+				MainFunctionParallel2(r[i][j], -F[i][j], w, i, j, M, N, a1, a2, b, h1, h2);
 			}
 		}
 		#pragma omp single nowait
@@ -149,12 +152,15 @@ int main(int argc, char **argv)
 		}
 		#pragma omp barrier
 		//посчитать итерационный параметр
-		#pragma omp for collapse(2) schedule(static) reduction(+:tauNumerator, tauDenominator)
-		for (i = 1; i < M; ++i)
+		#pragma omp for collapse(2) schedule(guided, 2) reduction(+:tauNumerator, tauDenominator)
+		for (i = M -1; i > 0; --i)
 		{
-			for (j = 1; j < N; ++j)
+			for (j = N -1; j > 0; --j)
 			{
-				MainFunctionParallel2(rA, 0, r, i, j, M, N, a, b, h1, h2);
+				double a1 = 0, a2 = 0;
+				CalcA(a1, i+1, j);
+				CalcA(a2, i, j);
+				MainFunctionParallel2(rA, 0, r, i, j, M, N, a1, a2, b, h1, h2);
 				tauNumerator += rA * r[i][j];
 				tauDenominator += rA * rA;
 			}
