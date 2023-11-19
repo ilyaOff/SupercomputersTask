2a929d8e32de71ac387138ec3260e91c628c6517
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2023-11-19 17:08:46 +0300

    feat: разделение функции
    не помогло, всё равно медленее

--- Task2/MyMacroses.h
+++ Task2/MyMacroses.h
@@ -15,13 +15,18 @@
 //left1 = i < 15 ? 2 : 5;\
 // res = -( w[i][j] + center1 + left1);};
 
-#define MainFunctionParallel2(res,f, w, i, j, M, N, a, b, h1, h2) {\
-	double center1, left1, right1, top1, down1;\
+#define MainFunctionParallelA(res,f, w, i, j, M, N, a, h1) {\
+	double center1, left1, right1;\
 	center1 = w[i][j];\
 	left1 = i - 1 == 0 ? 0 : w[i - 1][j];\
 	right1 = i + 1 == M ? 0 : w[i + 1][j];\
+	double dx = (a[j][i + 1] * (right1 - center1) - a[j][i] * (center1 - left1)) / (h1 * h1);\
+	res = f -dx;};
+
+#define MainFunctionParallelB(res,f, w, i, j, M, N, b, h2) {\
+	double center1, top1, down1;\
+	center1 = w[i][j];\
 	top1 = j + 1 == N ? 0 : w[i][j + 1];\
 	down1 = j - 1 == 0 ? 0 : w[i][j - 1];\
-	double dx = (a[j][i + 1] * (right1 - center1) - a[j][i] * (center1 - left1)) / (h1 * h1);\
 	double dy = (b[i][j + 1] * (top1 - center1) - b[i][j] * (center1 - down1)) / (h2 * h2);\
-	res = f -(dx + dy);};
+	res = f - dy;};
